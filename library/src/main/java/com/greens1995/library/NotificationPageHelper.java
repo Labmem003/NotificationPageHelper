62e6fbb24046e37eb6e3bf6e2f5697b06ba59313
Labmem003 <wenbin.private@gmail.com>
2017-12-04 14:52:04 +0800

    1.基本的 api 版本跳转，2.部分 oppo、vivo 的跳转

---
+++ library/src/main/java/com/greens1995/library/NotificationPageHelper.java
@@ -0,0 +1,48 @@
+package com.greens1995.library;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.util.Log;
+public class NotificationPageHelper {
+    /**
+     * 跳转到权限设置界面
+     */
+    public static void open(Context context){
+
+        // vivo 点击设置图标>加速白名单>我的app
+        //      点击软件管理>软件管理权限>软件>我的app>信任该软件
+        Intent appIntent = context.getPackageManager().getLaunchIntentForPackage("com.iqoo.secure");
+        if(appIntent != null){
+            context.startActivity(appIntent);
+            return;
+        }
+
+        // oppo 点击设置图标>应用权限管理>按应用程序管理>我的app>我信任该应用
+        //      点击权限隐私>自启动管理>我的app
+        appIntent = context.getPackageManager().getLaunchIntentForPackage("com.oppo.safe");
+        if(appIntent != null){
+            context.startActivity(appIntent);
+            return;
+        }
+
+        Intent intent = new Intent();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            intent.setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
+            intent.putExtra("app_package", context.getPackageName());
+            intent.putExtra("app_uid", context.getApplicationInfo().uid);
+        } else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD){
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            intent.setData(Uri.fromParts("package", context.getPackageName(), null));
+        } else if(Build.VERSION.SDK_INT < Build.VERSION_CODES.GINGERBREAD){
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setClassName("com.android.settings","com.android.settings.InstalledAppDetails");
+            intent.putExtra("com.android.settings.ApplicationPkgName", context.getPackageName());
+        }
+        context.startActivity(intent);
+    }
+}
