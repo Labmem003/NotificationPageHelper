ef2cd7d101696ffbdddfac7dbcdc10ddf50ba535
Labmem003 <wenbin.private@gmail.com>
2017-12-04 14:41:03 +0800

    init

---
+++ app/src/main/java/com/greens1995/notificationpagehelperapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.greens1995.notificationpagehelperapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
