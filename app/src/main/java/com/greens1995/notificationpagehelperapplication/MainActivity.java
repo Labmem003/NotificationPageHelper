1c3cf7c5b2bba610d0140800eabfaca55b29e13c
Labmem003 <wenbin.private@gmail.com>
2017-12-04 15:28:08 +0800

    1.add sample，2.modify minSdkVersion

--- app/src/main/java/com/greens1995/notificationpagehelperapplication/MainActivity.java
+++ app/src/main/java/com/greens1995/notificationpagehelperapplication/MainActivity.java
@@ -1,7 +1,12 @@
 package com.greens1995.notificationpagehelperapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.greens1995.library.NotificationPageHelper;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button goBtn = findViewById(R.id.btn_go);
+        goBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NotificationPageHelper.open(MainActivity.this);
+            }
+        });
     }
 }
